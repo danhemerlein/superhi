7645b6419997442d78f56a97674aa3c42ba64ea5
Dan <danhemerlein@gmail.com>
2019-03-31 23:32:04 -0400

    inital commit

---
+++ src/components/ProjectFour/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './ProjectFour.scss';
+
+class ProjectFour extends Component {
+ 
+  render() {
+    return (
+      <div className="ProjectFour">
+        Fuck
+      </div>
+    );
+  }
+}
+
+export default ProjectFour;
