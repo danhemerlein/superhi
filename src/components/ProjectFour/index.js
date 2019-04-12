a30aa3758f1deeb3475e3543d2a3102f98d0d746
Dan <danhemerlein@gmail.com>
2019-04-12 00:54:25 -0400

    completed project 4

--- src/components/ProjectFour/index.js
+++ src/components/ProjectFour/index.js
@@ -46,53 +46,116 @@ class ProjectFour extends Component {
       renderer.render(scene, camera)
       requestAnimationFrame(animate);
 
+      // move the camera closer
+      camera.position.setZ(camera.position.z + 1);
+
       // lets rotate the shapes each frame
       for (let shape of shapes) {
         shape.rotateX(0.005);
         shape.rotateY(0.005);
+
+        // move each individual shape closer BAD PRACTICE 
+        // shape.position.setZ(shape.position.z - 1);
       }
     };
 
     // start the animation
     animate();
 
+    let hue = 0;
+
     // lets make a function that creats a shape
     const createShape = function (x, y) {
+      const geometries = [
+        new THREE.ConeGeometry(10, 20, 30),
+        new THREE.BoxGeometry(15, 15, 15),
+        new THREE.TorusGeometry(5, 6, 16, 100),
+        new THREE.SphereGeometry(5, 32, 32),
+      ];
+
+      const randNumber = Math.floor(Math.random() * geometries.length);
+
       // geometry is the 'shape' of the shape
-      const geometry = new THREE.ConeGeometry(10, 15, 32);
+      const geometry = geometries[randNumber];
       // material is the thing that gets effected by the light
+
+      const emissiveColor = new THREE.Color(`hsl(${hue}, 100%, 50%)`)
+
       const material = new THREE.MeshLambertMaterial({
+        // white
         color: 0xffffff,
-        emissive: 0xff0000
+        // red
+        emissive: emissiveColor
       });
+
       const shape = new THREE.Mesh(geometry, material);
+
       // z index of the position makes the shape farther away
-      shape.position.set((window.innerWidth / 2) - x, (window.innerHeight / 2) - y, 400);
+      shape.position.set(
+        (window.innerWidth / 2) - x, 
+        (window.innerHeight / 2) - y,
+        // the shapes will always draw in the distance, 500 px from the camera, because the camera is moving towards the shapes, the shapes aren't moving towards the camera
+        camera.position.z + 500
+      );
       shape.rotateX(0.5);
       shape.rotateZ(0.5);
 
       // add the shape to the scene and the list of shapes 
       shapes.push(shape);
       scene.add(shape);
+
+      // update the hue that gets passed to the hsl color thingy
+      hue += 1;
+
+      console.log(shapes)
+
     }
 
-    // lets do things on a click
-    document.addEventListener('click', function(event) {
-      // creat the shape based on x and y
-      createShape(event.pageX, event.pageY);
+    let isMouseDown = false;
+
+    document.addEventListener('mousemove', function(event) {
+      // create the shape based on x and y
+      if (isMouseDown) {
+        createShape(event.pageX, event.pageY);
+      }
     })
-    createShape(50, 50);
+
+    document.addEventListener('mousedown', function (event) {
+      isMouseDown = true;
+    })
+
+    document.addEventListener('mouseup', function (event) {
+      isMouseDown = false;
+    })
+
+    // for mobile and tablet
+    document.addEventListener('touchmove', function (event) {
+      if (isMouseDown) {
+        createShape(event.pageX, event.pageY);
+      }
+    })
+
+    document.addEventListener('touchstart', function (event) {
+      isMouseDown = true;
+    })
+
+    document.addEventListener('touchend', function (event) {
+      isMouseDown = false;
+    })
+
+    window.addEventListener('resize', function() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      // must do when messing with the camera stuff
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    })
 
   }
   
   render() {
     return (
       <div className="ProjectFour">
-        <section id="ProjectFour__section">
-
-
-
-        </section>
+        <section id="ProjectFour__section"></section>
       </div>
     );
   }
