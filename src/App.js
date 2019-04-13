880a12fead36f07efd05c52a780821b262974883
Dan <danhemerlein@gmail.com>
2019-04-13 09:45:56 -0400

    set up comp for project five

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ProjectFour from "components/ProjectFour";
+import ProjectFive from "components/ProjectFive";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <div className="">
-            <Switch>
-              <Route exact path="/" component={ProjectFour} />
-            </Switch>
-          </div>
+          <Switch>
+            <Route exact path="/project4" component={ProjectFour} />
+            <Route exact path="/project5" component={ProjectFive} />
+          </Switch>
         </Router>
       </div>
     );
---
+++ src/components/ProjectFive/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import * as THREE from "three";
+
+import './ProjectFive.scss';
+
+class ProjectFive extends Component {
+
+  render() {
+    return (
+      <div className="ProjectFive">
+        oh word
+      </div>
+    );
+  }
+}
+
+export default ProjectFive;
